a0de03b57da147c757fa8b4ef6919f5b8e878981
nwebpro <abnaeem.bsc@gmail.com>
2026-08-03 22:07:56 +0600

    refactor: remove boilerplate code and implement Tailwind CSS with NativeWind styling

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        ratingBox: "#221F3D",
+        searchBar: "#0F0D23",
+        text: "#9CA4AB",
+        darkAccent: "#AB8BFF",
+        accentText: "#A8B5DB",
+        secondaryText: "#D6C7FF",
+      },
+    },
+  },
+  plugins: [],
+};
